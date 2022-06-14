19e245f2899f0e84e0c8faef9ad2ec9de5fcdb38
Amanda Smith <ajs.amandasmith@gmail.com>
2022-06-14 15:11:39 -0500

    Adds a new toy with the ToyForm component

--- src/components/App.js
+++ src/components/App.js
@@ -20,10 +20,14 @@ function App() {
     setShowForm((showForm) => !showForm);
   }
 
+  function submitNewToy(newToy) {
+    setToyData([...toyData, newToy]);
+  }
+
   return (
     <>
       <Header />
-      {showForm ? <ToyForm /> : null}
+      {showForm ? <ToyForm submitNewToy={submitNewToy} /> : null}
       <div className="buttonContainer">
         <button onClick={handleClick}>Add a Toy</button>
       </div>
@@ -40,10 +44,6 @@ App
   |__ToyForm
   |__ToyContainer
         |__ToyCard
-
-When our application loads, make a GET request to /toys to fetch the toy array. Given your component tree, think about which component should be responsible for the array. After you have put the data in the proper component, your next job is to render the ToyCard components on the page.
-
-When the ToyForm is submitted, make a POST request to /toys to save a new toy to the server. Using the ideas of controlled form and inverse data flow, think about how to render a new ToyCard for the toy that you created.
 
 When the Donate to Goodwill button is clicked, make a DELETE request to /toys/:id with the ID of the toy that was clicked to delete the toy from the server. The ToyCard that you clicked on should also be removed from the DOM.
 
--- src/components/ToyForm.js
+++ src/components/ToyForm.js
@@ -1,15 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 
-function ToyForm() {
+function ToyForm({ submitNewToy }) {
+  const [formData, setFormData] = useState({
+    name: "",
+    image: "",
+    likes: 0
+  })
+
+  function handleChange(event) {
+    setFormData({
+      ...formData,
+      [event.target.id]: event.target.value
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    fetch("http://localhost:3001/toys", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(formData)
+    })
+      .then(r => r.json())
+      .then(() => submitNewToy(formData))
+  }
+
+
   return (
     <div className="container">
-      <form className="add-toy-form">
+      <form className="add-toy-form" onSubmit={handleSubmit}>
         <h3>Create a toy!</h3>
         <input
           type="text"
           name="name"
           placeholder="Enter a toy's name..."
           className="input-text"
+          id="name"
+          value={formData.name}
+          onChange={handleChange}
         />
         <br />
         <input
@@ -17,6 +47,9 @@ function ToyForm() {
           name="image"
           placeholder="Enter a toy's image URL..."
           className="input-text"
+          id="image"
+          value={formData.image}
+          onChange={handleChange}
         />
         <br />
         <input
